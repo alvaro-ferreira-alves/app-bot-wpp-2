0567bb473c0193227cc3c58bf28a3c57dd5955d2
alvaro-ferreira-alves <77805712+alvaro-ferreira-alves@users.noreply.github.com>
2025-08-18 10:48:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
             buttons: [
                 'excel'
             ],
-            "ajax": "https://app-bot-wpp-4.onrender.com/api/read.php",
+            "ajax": "https://app-bot-wpp-5.onrender.com/api/read.php",
             "columns": [
                 {
                     "data": "pdv"
